4f05e05b8d3cda050fa8183ea10b7eb0878fd03b
Josh Hnath <joshhnath@gmail.com>
2015-10-28 11:25:43 -0400

    added more work samples

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
 $(function(){
 
-$(".work").click(function() {
-	alert("Thank you for checking out some of my work!");
+$(".work1").click(function() {
+	alert("This is an application for a pizza shop. Built with Rails.");
 });
+
+$(".work2").click(function() {
+	alert("This is a fully functional blogg application. Built with Rails.");
+});
+
+$(".work3").click(function() {
+	alert("This is a sample website built for a coffee shop. Built with Bootstrap.");
+});
+
+$(".work4").click(function() {
+	alert("This is an ATM simulator. Built with jQuery.");
+});
+
+$(".work5").click(function() {
+	alert("This is an old portfolio page. Built with Materialize.");
+});
+
+$(".work6").click(function() {
+	alert("This is a weather application. Built using the Wunderground API.");
+});
+
+$(".work7").click(function() {
+	alert("This is a product advertisement application. Built with Rails.");
+});
+
+$(".work8").click(function() {
+	alert("This is an image hosting application. Database is still a work in progress so it's not live yet. Built with Rails.");
+});
+
+$(".work9").click(function() {
+	alert("This is a restaurant finder application built by a team of two. This is not live at the moment. Built with Rails, Google API and JavaScript.");
+});
+
 });
